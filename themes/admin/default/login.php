3fbf1b5aad4616c5c8135518981e6dc7eabbaf04
hiroatsu <hinadome@yahoo.com>
2011-01-03 10:14:18 -0800

    login charset to utf8

--- themes/admin/default/login.php
+++ themes/admin/default/login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="Content-Type" content="text/html;  charset=UTF-8" />
 <title>68 KB Administration</title>
 <link href="<?php echo base_url();?>themes/admin/default/style/default.css" rel="stylesheet" type="text/css" />
 <script language="javascript" type="text/javascript">
